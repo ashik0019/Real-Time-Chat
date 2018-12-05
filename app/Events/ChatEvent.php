04e44c28aac44dea483dc6dc13831fbbcf29f057
Ashiqur Rahman <ashiq.starit@gmail.com>
2018-12-06 02:06:05 +0600

    first commit

---
+++ app/Events/ChatEvent.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use App\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class ChatEvent implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($message,User $user)
+    {
+        return $this->message = $message;
+        return $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('chat');
+    }
+}
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Real Time Chat </title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <style>
+        .list-group{
+            overflow: scroll;
+            max-height: 200px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="row mt-5" id="app">
+        <div class="offset-4 col-4">
+            <li class="list-group-item active">Chat Room</li>
+            <ul class="list-group" v-chat-scroll>
+                <message
+                    v-for="value in chat.message"
+                    :key="value.index"
+                    color="success"
+                >@{{value}}
+                </message>
+            </ul>
+            <input type="text" v-model="message" @keyup.enter="send()" class="form-control" placeholder="Type your message here..">
+        </div>
+    </div>
+</div>
+
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
